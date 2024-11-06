bcffba16c0ded0fffd8f1c6c970a00fc142400f3
Carsten Flokstra <carsten.flokstra@nedap.com>
2024-11-06 21:15:49 +0100

    Trying other Github Deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: './',
-    build: {
-        outDir: 'dist',
-        copyPublicDir: true,
-        emptyOutDir: true,
-    },
-    css: {
-        devSourcemap: true,
-    },
+    base: "/", // YOUR REPO NAME HERE
 });
